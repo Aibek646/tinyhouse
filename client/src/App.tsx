c123fe4cbce63344ea5219e38c383cb080e37e9d
Aibek Ramazanov <aibekramazanov@Aibeks-MacBook-Pro.local>
2023-05-07 19:54:09 -0600

    front end working on fetching and deleting data through GraphQL

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Listings } from "./sections";
+
+function App() {
+    return (
+        <div className="App">
+            <Listings title="TinyHouse Listing" />
+        </div>
+    );
+}
+
+export default App;
---
+++ client/src/sections/Listings/Listings.tsx
@@ -0,0 +1,62 @@
+import { server } from "../../lib/api";
+import {
+    DeleteListingData,
+    DeleteListingVariables,
+    ListingsData
+} from "./types";
+
+const LISTINGS = `
+    
+query Listings {
+    listings {
+        id
+        title
+        image
+        address
+        price
+        numOfGuests
+        numOfBeds
+        numOfBaths
+        rating
+    }
+}
+`;
+
+const DELETE_LISTING = `
+    mutation DeleteListing($id: ID!) {
+        deleteListing(id: $id) {
+            id
+        }
+    }
+`;
+
+interface Props {
+    title: string;
+}
+
+export const Listings = ({ title }: Props) => {
+    const fetchListings = async () => {
+        const { data } = await server.fetch<ListingsData>({ query: LISTINGS });
+        console.log(data);
+    };
+
+    const deleteListing = async () => {
+        const { data } = await server.fetch<
+            DeleteListingData,
+            DeleteListingVariables
+        >({
+            query: DELETE_LISTING,
+            variables: {
+                id: ""
+            }
+        });
+    };
+
+    return (
+        <div>
+            <h2>{title}</h2>
+            <button onClick={fetchListings}>Query Listings!</button>
+            <button onClick={deleteListings}>Delete a listing!</button>
+        </div>
+    );
+};
