22b78922a9135d7b78d15e82ccb50f951c16010e
Aibek Ramazanov <aibekramazanov@Aibeks-MacBook-Pro.local>
2023-05-08 20:34:14 -0600

    useQuery hook

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -1,5 +1,4 @@
-import { useEffect, useState } from "react";
-import { server } from "../../lib/api";
+import { server, useQuery } from "../../lib/api";
 import {
     DeleteListingData,
     DeleteListingVariables,
@@ -36,16 +35,7 @@ interface Props {
 }
 
 export const Listings = ({ title }: Props) => {
-    const [listings, setListings] = useState<Listing[] | null>(null);
-
-    useEffect(() => {
-        fetchListings();
-    }, []);
-
-    const fetchListings = async () => {
-        const { data } = await server.fetch<ListingsData>({ query: LISTINGS });
-        setListings(data.listings);
-    };
+    const { data } = useQuery<ListingsData>(LISTINGS);
 
     const deleteListing = async (id: string) => {
         await server.fetch<DeleteListingData, DeleteListingVariables>({
@@ -54,8 +44,9 @@ export const Listings = ({ title }: Props) => {
                 id: id
             }
         });
-        fetchListings();
     };
+
+    const listings = data ? data.listings : null;
 
     const listingList = listings ? (
         <ul>
