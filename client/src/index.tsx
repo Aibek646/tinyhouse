242c4a97533eab6cb28b58af65ae531fd08683a7
Aibek Ramazanov <aibekramazanov@Aibeks-MBP.lan>
2023-06-17 00:07:19 -0500

    creating user query

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,15 @@
 import ReactDOM from "react-dom/client";
 import "antd/dist/reset.css";
+import { useState, useEffect, useRef } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import "./styles/index.css";
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import {
+    ApolloClient,
+    ApolloProvider,
+    InMemoryCache,
+    useMutation
+} from "@apollo/client";
 import { Viewer } from "./newlib/types";
 import {
     AppHeader,
@@ -14,13 +21,21 @@ import {
     User,
     Login
 } from "./sections";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Layout, Affix } from "antd";
-import { useState } from "react";
+
+import { Layout, Affix, Spin } from "antd";
+import { LOG_IN } from "./newlib/graphql/mutations";
+import {
+    LogIn as LogInData,
+    LogInVariables
+} from "./newlib/graphql/mutations/LogIn/__generated__/LogIn";
+import { AppHeaderSkeleton, ErrorBanner } from "./newlib/components";
 
 const client = new ApolloClient({
     uri: "/api",
-    cache: new InMemoryCache()
+    cache: new InMemoryCache(),
+    headers: {
+        "X-CSRF-TOKEN": sessionStorage.getItem("token") || ""
+    }
 });
 
 const initialViewer: Viewer = {
@@ -38,9 +53,44 @@ const root = ReactDOM.createRoot(
 const App = () => {
     const [viewer, setViewer] = useState<Viewer>(initialViewer);
 
+    const [logIn, { error }] = useMutation<LogInData, LogInVariables>(LOG_IN, {
+        onCompleted: (data) => {
+            if (data && data.logIn) {
+                setViewer(data.logIn);
+                if (data.logIn.token) {
+                    sessionStorage.setItem("token", data.logIn.token);
+                } else {
+                    sessionStorage.removeItem("token");
+                }
+            }
+        }
+    });
+
+    const logInRef = useRef(logIn);
+
+    useEffect(() => {
+        logInRef.current();
+    }, []);
+
+    if (!viewer.didRequest && !error) {
+        return (
+            <Layout className="app-skeleton">
+                <AppHeaderSkeleton />
+                <div className="app-skeleton__spin-section">
+                    <Spin size="large" tip="Launcing Tinyhouse" />
+                </div>
+            </Layout>
+        );
+    }
+
+    const logInErrorBannerElement = error ? (
+        <ErrorBanner description="We weren't able to verify if you were logged in. Please try again soon!" />
+    ) : null;
+
     return (
         <Router>
             <Layout id="app">
+                {logInErrorBannerElement}
                 <Affix offsetTop={0} className="app_affix-header">
                     <AppHeader viewer={viewer} setViewer={setViewer} />
                 </Affix>
